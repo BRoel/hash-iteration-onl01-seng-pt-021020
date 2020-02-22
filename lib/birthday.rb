5bc39940663a5c7c8e22e1a32264a4a87c332359
Brennan Roel <brennanroel@gmail.com>
2020-02-22 18:46:17 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,3 +13,4 @@ end
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
   puts "Happy Birthday #{kids_name}! You are now #{age} years old!\nYou are too old for this."
+end
