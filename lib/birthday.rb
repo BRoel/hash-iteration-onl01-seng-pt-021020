72b93b635e63ddcff3007585f483785d3c0fb0f7
Brennan Roel <brennanroel@gmail.com>
2020-02-22 18:32:18 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -2,10 +2,10 @@
 #   "Timmy" => 9, 
 #   "Sarah" => 6, 
 #   "Amanda" => 27
-# }
+}
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+  puts "Happy Birthday #{birthday_kids}! You are now #{birthday_kids[]}"
 end
 
 
